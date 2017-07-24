eb816a7860ee75a074d05bea48873de011b45f06
Gareth Heyes <gareth.heyes@portswigger.net>
2017-07-24 11:51:23 +0100

    Fixed Firefox cross domain issues with length property

--- js/inspector.js
+++ js/inspector.js
@@ -156,12 +156,14 @@ window.inspector = function(){
     output += '</td>';
     output += '<td valign="top" class="propertyInfo">';
     output += '<div class="box '+escapeHTML(realType)+'">type:'+escapeHTML(realType)+'</div>';
-    if(typeof obj === 'function') {
-      output += '<div class="box arguments">arguments:'+escapeHTML(obj.length)+'</div>';
-      output += '<div class="box"><a href="#" onclick="try{'+escapeHTML(generatePath(path))+'(function(){alert(\'Callback called\');});}catch(e){alert(e);}return false;">Call function with callback</a></div>';
-    } else if(obj && typeof obj.length !== 'undefined') {
-      output += '<div class="box length">length:'+escapeHTML(obj.length)+'</div>';
-    }
+    try {
+      if(typeof obj === 'function') {
+        output += '<div class="box arguments">arguments:'+escapeHTML(obj.length)+'</div>';
+        output += '<div class="box"><a href="#" onclick="try{'+escapeHTML(generatePath(path))+'(function(){alert(\'Callback called\');});}catch(e){alert(e);}return false;">Call function with callback</a></div>';
+      } else if(obj && typeof obj.length !== 'undefined') {
+        output += '<div class="box length">length:'+escapeHTML(obj.length)+'</div>';
+      }
+    } catch(e){}
     if(Object.getOwnPropertyDescriptor && parent) {
       try {
         descriptor = Object.getOwnPropertyDescriptor(parent, name);
