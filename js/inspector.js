9ad00b6e4fbfdf800267501726fad4b3c00f163b
Gareth Heyes <gareth.heyes@portswigger.net>
2017-07-24 10:46:55 +0100

    Created call function with callback link

--- js/inspector.js
+++ js/inspector.js
@@ -158,6 +158,7 @@ window.inspector = function(){
     output += '<div class="box '+escapeHTML(realType)+'">type:'+escapeHTML(realType)+'</div>';
     if(typeof obj === 'function') {
       output += '<div class="box arguments">arguments:'+escapeHTML(obj.length)+'</div>';
+      output += '<div class="box"><a href="#" onclick="try{'+escapeHTML(generatePath(path))+'(function(){alert(\'Callback called\');});}catch(e){alert(e);}return false;">Call function with callback</a></div>';
     } else if(obj && typeof obj.length !== 'undefined') {
       output += '<div class="box length">length:'+escapeHTML(obj.length)+'</div>';
     }
