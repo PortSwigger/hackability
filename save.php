a9a5b87c4ba3ad403b7a627228d2fe2eb7901937
Gareth Heyes <gareth.heyes@portswigger.net>
2017-08-04 08:45:09 +0100

    Added sqlite functionality to store blind enumerations

---
+++ save.php
@@ -0,0 +1,13 @@
+<?php
+require("inspectorLogger.class.php");
+$objName = isset($_POST['objName']) ? $_POST['objName'] : '';
+$html = isset($_POST['html']) ? $_POST['html'] : '';
+$logger = new InspectorLogger();
+$logger->init();
+if($logger) {
+  $logger->insertData($objName, $html);
+  $logger->close();
+} else {
+  echo 'Unable to open database.';
+}
+?>
